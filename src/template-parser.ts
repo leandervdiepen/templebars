63e884d67cf7a4730a5a31f1ea99c04ee3464806
Leander van Diepen <leander.vandiepen@check24.de>
2023-12-28 07:22:59 +0100

    test cases multiple variables and complex html and updated implementation

--- src/template-parser.ts
+++ src/template-parser.ts
@@ -2,8 +2,17 @@ const parseHandlebar = (template: string) => {
     return template.replace(/{{/g, '{').replace(/}}/g, '}');
 }
 
-const fillTemplate = (template: string, data: any) => {
-    return template.replace(/{{/g, '').replace(/}}/g, '').replace(/name/g, data.name);
+const fillTemplate = (template: string, data: { [key: string]: string }) => {
+    const keys = Object.keys(data);
+    console.log("keys", keys);
+
+    keys.forEach(key => {
+        const regex = new RegExp(`{{${key}}}`, 'g');
+        template = template.replace(regex, data[key]);
+ 
+    });
+
+    return template;
 }
 
 export { parseHandlebar, fillTemplate };
--- tests/template.test.ts
+++ tests/template.test.ts
@@ -24,3 +24,15 @@ test("fill template with multiple variables", () => {
     const expected = '<div>John Smith</div>';
     expect(fillTemplate(template, {name: "John", surname: "Smith"})).toBe(expected);
 })
+
+test("fill complex html structure with multiple variables", () => {
+    const template = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>{{title}}</title></head><body><h1>{{hTitle}}</h1><h2>{{subtitle}}</h2><div>{{name}} {{surname}}</div></body></html>';
+    const expected = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Hello</title></head><body><h1>Hello</h1><h2>World</h2><div>John Smith</div></body></html>';
+    expect(fillTemplate(template, {name: "John", surname: "Smith", title: "Hello", hTitle: "Hello", subtitle: "World"})).toBe(expected);
+})
+
+test("fill template with multiple variables and multiple occurrences", () => {
+    const template = '<div>{{name}} {{surname}} {{name}} {{surname}}</div>';
+    const expected = '<div>John Smith John Smith</div>';
+    expect(fillTemplate(template, {name: "John", surname: "Smith"})).toBe(expected);
+})
