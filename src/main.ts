db8098c387d8ac9a8d04182692b5b375bc2eff6c
Leander van Diepen <leander.vandiepen@check24.de>
2023-12-28 08:13:25 +0100

    vite + config

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 const hello = (name: string) => {
-    return `Hello there, ${name}!`;
-  };
-  
-export { hello };
+  return `Hello there, ${name}!`;
+};
+
+const fillTemplate = import("./template-parser").then(
+  (module) => module.fillTemplate
+);
+export { hello, fillTemplate };
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,43 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+import typescript from "@rollup/plugin-typescript";
+import path from "path";
+import { typescriptPaths } from "rollup-plugin-typescript-paths";
+
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
+  plugins: [],
+  resolve: {
+    alias: [
+      {
+        find: "~",
+        replacement: path.resolve(__dirname, "./src"),
+      },
+    ],
   },
-})
+  server: {
+    port: 3000,
+  },
+  build: {
+    manifest: true,
+    minify: true,
+    reportCompressedSize: true,
+    lib: {
+      entry: path.resolve(__dirname, "src/main.ts"),
+      fileName: "main",
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: [],
+      plugins: [
+        typescriptPaths({
+          preserveExtensions: true,
+        }),
+        typescript({
+          sourceMap: false,
+          declaration: true,
+          outDir: "dist",
+        }),
+      ],
+    },
+  },
+});
